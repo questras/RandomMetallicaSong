30034f8224165986644a3455d152b3ec526d63c7
questras <michu123qwe@gmail.com>
2020-05-02 16:25:06 +0200

    fix small bug with __str__ method of Song

--- RandomMetallicaSong/songpicker/models.py
+++ RandomMetallicaSong/songpicker/models.py
@@ -6,4 +6,4 @@ class Song(models.Model):
     album = models.CharField(max_length=200)
 
     def __str__(self):
-        return f'{self.song_name} of {self.album}'
+        return f'{self.name} --- {self.album}'
