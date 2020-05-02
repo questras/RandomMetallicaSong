da76622befc28b3fa58618591b54bc35ead6e7d7
questras <michu123qwe@gmail.com>
2020-05-02 16:14:14 +0200

    change Song model's name to be unique

--- RandomMetallicaSong/songpicker/models.py
+++ RandomMetallicaSong/songpicker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Song(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     album = models.CharField(max_length=200)
 
     def __str__(self):
