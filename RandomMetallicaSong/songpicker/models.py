3ee9d5425065e87aae0e10110c22a635585f3066
questras <michu123qwe@gmail.com>
2020-05-02 15:51:20 +0200

    update song model

--- RandomMetallicaSong/songpicker/models.py
+++ RandomMetallicaSong/songpicker/models.py
@@ -2,13 +2,8 @@ from django.db import models
 
 
 class Song(models.Model):
-    song_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     album = models.CharField(max_length=200)
-    youtube_link = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.song_name
-
-    def embed_link(self):
-        embed_link = 'https://www.youtube.com/embed/' + self.youtube_link[32:]
-        return embed_link
+        return f'{self.song_name} of {self.album}'
