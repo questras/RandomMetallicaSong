92704f6d41e1b731c56f05190cbe7200f4166cee
questras <michu123qwe@gmail.com>
2020-05-02 16:26:07 +0200

    add database with all metallica songs setup

---
+++ RandomMetallicaSong/songpicker/database_with_songs_setup.py
@@ -0,0 +1,19 @@
+"""
+Module to add to database all scraped Metallica songs.
+Run 'setup' function from manage.py shell.
+"""
+from .models import Song
+from .songs_scraper import get_songs_list
+
+
+def setup():
+    """
+    Add all Metallica songs from [get_songs_list] to database.
+    :return:
+    """
+    songs_list = get_songs_list()
+    for idx, (name, album) in enumerate(songs_list):
+        print(f'Creating Song: {name} {album}')
+        obj = Song(name=name, album=album)
+        obj.save()
+        print(f'Song created.')
