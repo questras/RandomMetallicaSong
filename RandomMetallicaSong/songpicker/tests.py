8d3aec0172f20543b311d4bf1c7681ac6071b11f
questras <michu123qwe@gmail.com>
2020-05-04 18:38:43 +0200

    add tests for index, pick and song_view

--- RandomMetallicaSong/songpicker/tests.py
+++ RandomMetallicaSong/songpicker/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from .database_with_songs_setup import setup_database_with_songs
+import time
 
-# Create your tests here.
+from .models import Song
+
+
+class IndexViewTests(TestCase):
+    def test_can_access_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class PickViewTests(TestCase):
+    def test_view_redirects(self):
+        setup_database_with_songs()
+        response = self.client.get(reverse('pick'))
+        self.assertEqual(response.status_code, 302)
+
+
+class SongViewTests(TestCase):
+    def test_view_for_every_song(self):
+        """
+        Test if song_view can be accessed for each song in database.
+        Each request to song_view sends a request to youtube page, so
+        test for each song is made every 1 second.
+        Approximate time of test: 3 min.
+        :return:
+        """
+        setup_database_with_songs()
+        songs = Song.objects.all()
+
+        print("\nRunning song_view test for every song.")
+        print("This may take a while (approx. 5min).")
+        for song in songs:
+            url = reverse('song_view', kwargs={'song_id': song.pk})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            time.sleep(1)
